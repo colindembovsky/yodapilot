376e9839ae94a121706a15e3cdb8f7b5bd01c859
Colin Dembovsky <colindembovsky@github.com>
2024-09-25 13:13:20 -0500

    Complete "hello world"

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { createAckEvent, createDoneEvent, createErrorsEvent, getUserMessage } from "@copilot-extensions/preview-sdk";
 import { Octokit } from '@octokit/rest';
+import { Readable } from 'stream';
 
 export class IndexController {
   constructor() {
@@ -8,59 +9,43 @@ export class IndexController {
   }
 
   async postIndex(req: Request, res: Response) {
-    console.log("Got post to index");
+    //console.log("Got post to index");
 
     res.write(createAckEvent());
     await this.doYoda(req, res);
   }
 
   async doYoda(req: Request, res: Response) {
-    let user = "test-user";
-    const tokenForUser = req.get("X-GitHub-Token");
-    try {
-      const octokit = new Octokit({ auth: tokenForUser });
-      const userObj = await octokit.request("GET /user");
-      const user = userObj.data.login;
-      console.log("User:", user);
-    } catch (e) {
-      //console.log("Error getting user:", e);
-    }
+    let { tokenForUser, user } = await this.getTokenAndUsername(req);
 
     const payload = req.body;
-    console.log(`Payload: ${JSON.stringify(payload)}`);
-
-    const messages = (payload && Object.keys(payload).length === 0 && payload.constructor === Object) || !payload.messages ? [] : payload.messages;
-    messages.unshift({
-      role: "system",
-      content: "You are a helpful assistant that replies to user messages as if you were Yoda.",
-    });
-    messages.unshift({
-      role: "system",
-      content: `Start every response with the user's name, which is @${user}.`,
-    });
-
     if (payload && payload.messages) {
       const userMessage = getUserMessage(payload);
-      console.log(`User message: ${userMessage}`);
+      //console.log(`User message: ${userMessage}`);
     }
 
-    const copilotLLMResponse = await fetch(
-      "https://api.githubcopilot.com/chat/completions",
-      {
-        method: "POST",
-        headers: {
-          authorization: `Bearer ${tokenForUser}`,
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          messages,
-          stream: true,
-        }),
-      }
-    );
+    const messages = this.prepareMessages(payload, user);
+    const copilotLLMResponse = await this.fetchCopilotLLMResponse(tokenForUser, messages);
+    await this.handleCopilotLLMResponse(copilotLLMResponse, res);
+  }
 
+  async fetchCopilotLLMResponse(tokenForUser: string, messages: any) {
+    return await fetch("https://api.githubcopilot.com/chat/completions", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${tokenForUser}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        messages,
+        stream: true,
+      }),
+    });
+  }
+
+  async handleCopilotLLMResponse(copilotLLMResponse: any, res: any) {
     if (copilotLLMResponse.body) {
-      console.log(`Status from Copilot LLM: ${copilotLLMResponse.status}`);
+      //console.log(`Status from Copilot LLM: ${copilotLLMResponse.status}`);
       if (!copilotLLMResponse.ok) {
         const errorText = await copilotLLMResponse.text();
         console.log(`Error from Copilot LLM: ${errorText}`);
@@ -76,8 +61,8 @@ export class IndexController {
         res.end(createDoneEvent());
         return;
       }
-      console.log("Got 200-ish - piping to client");
-      copilotLLMResponse.body.pipe(res);
+      //console.log("Got ok from LLM - piping to client");
+      Readable.from(copilotLLMResponse.body as any).pipe(res);
     } else {
       const errorEvent = createErrorsEvent([
         {
@@ -90,5 +75,32 @@ export class IndexController {
       res.write(errorEvent);
       res.end(createDoneEvent());
     }
+  }
+
+  prepareMessages(payload: any, user: string): any[] {
+    const messages = (payload && Object.keys(payload).length === 0 && payload.constructor === Object) || !payload.messages ? [] : payload.messages;
+    messages.unshift({
+      role: "system",
+      content: "You are a helpful assistant that replies to user messages as if you were Yoda.",
+    });
+    messages.unshift({
+      role: "system",
+      content: `Start every response with the user's name, which is @${user}.`,
+    });
+    return messages;
+  }
+
+  async getTokenAndUsername(req: Request): Promise<{ tokenForUser: string, user: string }> {
+    let user = "test-user";
+    const tokenForUser = req.get("X-GitHub-Token") ?? "";
+    try {
+      const octokit = new Octokit({ auth: tokenForUser });
+      const userObj = await octokit.request("GET /user");
+      user = userObj.data.login;
+      //console.log("User:", user);
+    } catch (e) {
+      //console.log("Error getting user:", e);
+    }
+    return { tokenForUser, user };
   }
 }
